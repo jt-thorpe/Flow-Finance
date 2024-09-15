d1ee58f7380c5cb0c8fb8059b48b18bd9a119956
jt-thorpe <jtt-github@proton.me>
2024-09-15 12:31:05 +0100

    Setup initial flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welcome to My Budget App"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
