5e26149516d64a49d538c91743b5018f8561bf9e
jt-thorpe <jtt-github@proton.me>
2024-09-20 15:31:05 +0100

    Update login route

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import (Flask, jsonify, redirect, render_template, request, session,
                    url_for)
+from flask.typing import ResponseClass
 
+from flow.backend.authentication.auth import authenticate
 from flow.backend.postgresql.connect import get_db_connection
+from flow.backend.postgresql.database import Base, engine, get_session
+from flow.backend.postgresql.models import User
 
 app = Flask(__name__,
             template_folder='/app/flow/frontend/templates/',
             static_folder='/app/flow/frontend/static/')  # prepend /app/ for Docker
 
-
-# Dummy user data for example purposes
-users = {'user@example.com': 'password123'}
+Base.metadata.create_all(engine)
 
 
-@app.route('/')
+@app.route('/home')
 def home():
     conn = get_db_connection()
 
@@ -27,37 +29,22 @@ def home():
     return render_template('index.html', title="My Budget App", db_version=db_version)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        data = request.get_json()
-
-        email = data.get('email')
-        password = data.get('password')
-
-        # Check if user exists and password is correct
-        if email in users and users[email] == password:
-            session['user'] = email
-            return jsonify({'success': True}), 200
-        else:
-            return jsonify({'success': False, 'message': 'Invalid email or password'}), 401
-
+@app.route('/login', methods=['GET'])
+def login_serve() -> str:
+    """Serve `login.html` page."""
+    # TODO: Implement some kind of check for a token etc so don't always have to login if token valid
     # GET request: serve login page
     return render_template('login.html')
 
 
-@app.route('/dashboard')
-def dashboard():
-    # Only allow access to dashboard if the user is logged in
-    if 'user' in session:
-        return f'Hello, {session["user"]}! Welcome to the dashboard.'
-    return redirect(url_for('login'))
+@app.route('/login', methods=['POST'])
+def login_authenticate() -> ResponseClass:
+    data = request.json
+    user = authenticate(data['username'], data['password'])
 
-
-@app.route('/logout')
-def logout():
-    session.pop('user', None)  # Remove the user from session
-    return redirect(url_for('login'))
+    if user:
+        return redirect(url_for('home'))
+    return jsonify({'message': 'Invalid credentials'}), 401
 
 
 if __name__ == '__main__':
