9082f2f721741937bdf1b17f8eb1ef5b91965219
jt-thorpe <jtt-github@proton.me>
2024-09-22 10:10:03 +0100

    chore: moved DeclaratveBase class to models.py

--- flow/backend/postgresql/database.py
+++ flow/backend/postgresql/database.py
@@ -11,10 +11,8 @@ import os
 
 import psycopg2
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()  # Base class for DB models
 engine = create_engine(url=os.environ["FLOW_DB_URI"], echo=True)
 Session = sessionmaker(bind=engine)
 
