e97fd7ea45ced659e66cb40e39af8802b61d84f3
jt-thorpe <jtt-github@proton.me>
2024-09-22 10:06:09 +0100

    chore: improve User class
    - associated table renamed to `user_account` to avoid clash with psql reserved word `user`
    - add additional constraints to User.id

--- flow/backend/postgresql/models.py
+++ flow/backend/postgresql/models.py
@@ -1,14 +1,30 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, MappedAsDataclass, mapped_column
+import uuid
 
-from flow.backend.postgresql.database import Base
+from sqlalchemy import String, text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import (DeclarativeBase, Mapped, MappedAsDataclass,
+                            mapped_column)
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 class User(MappedAsDataclass, Base):
-    """A 'User' dataclass which is mapped via ORM to a 'user' table."""
+    """A 'User' dataclass which is mapped via ORM to a 'user' table.
 
-    __tablename__ = "user"
+    A value for User.id should not be passed when creating a User. The generation of
+    User.id is handled explicitly by the database.
+    """
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(255))
-    password: Mapped[str] = mapped_column(String(30))
+    __tablename__ = "user_account"
+
+    # `init=False` ensures we cannot pass `id` when instantiating a User
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),
+                                          init=False,
+                                          primary_key=True,
+                                          unique=True,
+                                          server_default=text("gen_random_uuid()"))
+    email: Mapped[str] = mapped_column(String(100),
+                                       unique=True)
+    password: Mapped[str] = mapped_column(String(100))
