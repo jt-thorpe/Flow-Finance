efc38cbaaa58abb705a3a1e854dcd1f6b465986c
jt-thorpe <jtt-github@proton.me>
2024-09-24 10:08:09 +0100

    feat: Add transaction model and update dashboard
    - Add transaction model to database
    - Update dashboard to display transactions from a user

--- flow/backend/postgresql/models.py
+++ flow/backend/postgresql/models.py
@@ -1,6 +1,7 @@
 import uuid
+from typing import List
 
-from sqlalchemy import String, text
+from sqlalchemy import Date, Float, ForeignKey, String, text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import (DeclarativeBase, Mapped, MappedAsDataclass,
                             mapped_column)
@@ -19,12 +20,37 @@ class User(MappedAsDataclass, Base):
 
     __tablename__ = "user_account"
 
-    # `init=False` ensures we cannot pass `id` when instantiating a User
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),
                                           init=False,
                                           primary_key=True,
                                           unique=True,
                                           server_default=text("gen_random_uuid()"))
-    email: Mapped[str] = mapped_column(String(100),
-                                       unique=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
+
+    # Saw this in an SQLAlchemy example, but not sure if I want this behavior
+    # transactions: Mapped[List["Transaction"]] = relationship(back_populates="user")  # user field in Transaction
+
+
+class Transaction(MappedAsDataclass, Base):
+    """A 'Transaction' dataclass which is mapped via ORM to a 'transaction' table.
+
+    A value for Transaction.id should not be passed when creating a Transaction. The generation of
+    Transaction.id is handled explicitly by the database.
+    """
+
+    __tablename__ = "transaction"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),
+                                          init=False,
+                                          primary_key=True,
+                                          unique=True,
+                                          server_default=text("gen_random_uuid()"))
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user_account.id"), nullable=False)
+    amount: Mapped[float] = mapped_column(Float, nullable=False)
+    description: Mapped[str] = mapped_column(String(100), nullable=True)
+    date: Mapped[Date] = mapped_column(Date, nullable=False)  # Check Date type is appropriate
+    category: Mapped[str] = mapped_column(String(100), nullable=True)
+
+    # Saw this in an SQLAlchemy example, but not sure if I want this behavior
+    # user: Mapped["User"] = relationship(back_populates="transactions")  # transactions field in User
