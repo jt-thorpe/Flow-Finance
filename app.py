2db537d902249dbfdcea03aa0c97c3b2707c4c0f
jt-thorpe <jtt-github@proton.me>
2024-09-24 10:05:14 +0100

    feat: add jwt and cookie authentication
    Realise that to get transactions from the DB, we need to know who the user is. Add JWT and cookie authentication to the backend to track the user.
    
    - Add generate_token and verify_token functions to auth.py to allow tracking of the user
    - Add @login_required decorator to app.py to require authentication for certain routes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify, render_template, request, url_for
+from flask import (Flask, jsonify, make_response, render_template, request,
+                   url_for)
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 
-from flow.backend.authentication.auth import (authenticate,
+from flow.backend.authentication.auth import (_hash_password, authenticate,
+                                              generate_token, login_required,
                                               register_user_account)
-from flow.backend.postgresql.database import engine, get_db_connection
-from flow.backend.postgresql.models import Base
+from flow.backend.postgresql.database import (engine, get_db_connection,
+                                              get_session,
+                                              get_user_transactions)
+from flow.backend.postgresql.models import Base, Transaction, User
 
 app = Flask(__name__,
             template_folder='/app/flow/frontend/templates/',
@@ -12,6 +17,34 @@ app = Flask(__name__,
 
 
 Base.metadata.create_all(engine)
+
+
+# ###
+# # Add some dummy data to the database
+# session = get_session()
+
+# # h_password = _hash_password("password")
+# # test_user = User(email="example@mail.com", password=h_password)
+# # session.add(test_user)
+# # session.commit()
+
+# test_user_id = session.execute(
+#     select(User.id).where(User.email == "example@mail.com")
+# ).first()
+# print(f"TEST_USER_ID_HERE =============== {test_user_id}")
+
+# test_transaction_1 = Transaction(user_id=test_user_id[0], amount=100.00,
+#                                  description="Test transaction 1", date="2021-01-01", category="food")
+# test_transaction_2 = Transaction(user_id=test_user_id[0], amount=50.00,
+#                                  description="Test transaction 2", date="2021-01-02", category="drink")
+# test_transaction_3 = Transaction(user_id=test_user_id[0], amount=75.00,
+#                                  description="Test transaction 3", date="2021-01-03", category="utility")
+# session.add(test_transaction_1)
+# session.add(test_transaction_2)
+# session.add(test_transaction_3)
+# session.commit()
+# session.close()
+# ###
 
 
 @app.route('/register', methods=['GET'])
@@ -41,21 +74,31 @@ def login_serve() -> str:
 @app.route('/login', methods=['POST'])
 def login_authenticate():
     data = request.json
-    user = authenticate(data['email'], data['password'])
+    user_id = authenticate(data['email'], data['password'])
+    print(f"USER =============== {user_id}")
 
-    if user:
-        return jsonify({'success': True, 'redirect': url_for('dashboard_serve')}), 200
+    if user_id:
+        token = generate_token(user_id=user_id)
+        print(f"TOKEN =============== {token}")
+        response = make_response(jsonify({'success': True, 'redirect': url_for('dashboard_serve')}), 200)
+        response.set_cookie('jwt_token', token, httponly=True, secure=True, samesite='Strict')
+        return response
     return jsonify({'success': False, 'message': 'Invalid credentials'}), 401
 
 
 @app.route('/dashboard', methods=['GET'])
+@login_required
 def dashboard_serve() -> str:
     """Serve `dashboard.html` page."""
-    return render_template('dashboard.html')
+    user_id = request.user_id
+    return render_template('dashboard.html',
+                           transactions=get_user_transactions(user_id=user_id))
 
 
 @app.route('/home')
+@login_required
 def home():
+    # TODO: Eventually will be the homepage of the website.
     conn = get_db_connection()
 
     cur = conn.cursor()
