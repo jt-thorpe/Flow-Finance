0b1fe59afda17896170350053a86b69f950e5f08
jt-thorpe <jtt-github@proton.me>
2024-09-27 13:31:57 +0100

    feat: add get_budgets_by_by(user_id) query

--- flow/backend/postgresql/queries.py
+++ flow/backend/postgresql/queries.py
@@ -1,5 +1,5 @@
 from extensions import db
-from flow.backend.postgresql.models import Transaction, User
+from flow.backend.postgresql.models import Budget, Transaction, User
 
 
 def get_n_transactions(user_id: str, N: int = 10) -> list[Transaction]:  # Hint might be wrong
@@ -22,3 +22,22 @@ def get_n_transactions(user_id: str, N: int = 10) -> list[Transaction]:  # Hint 
     )
 
     return transactions
+
+
+def get_budgets_by(user_id: str) -> list[Budget]:
+    """Get all budgets for a user.
+
+    Args:
+        user_id, str: the UUID of the user taken from the JWT token.
+
+    Returns:
+        list[Budget]: a list of Budget objects.
+    """
+    budgets = (
+        db.session.query(Budget)
+        .join(User)
+        .filter(User.id == user_id)
+        .all()
+    )
+
+    return budgets
