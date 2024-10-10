f0bb566fab4de5afaf342c281b436fe77dffb630
jt-thorpe <jtt-github@proton.me>
2024-10-10 14:03:59 +0100

    refactor: to reflect new model structure

--- scripts/populate.py
+++ scripts/populate.py
@@ -3,54 +3,143 @@ from sqlalchemy import select, text
 from app import app
 from extensions import db
 from flow.backend.authentication.auth import _hash_password
-from flow.backend.postgresql.models import (Budget, CategoryName, Transaction,
-                                            User)
+from flow.backend.postgresql.enums import (ExpenseCategory, Frequency,
+                                           IncomeCategory)
+from flow.backend.postgresql.models import (Budget, Expense, ExpenseCategory,
+                                            Income, User)
 
-"""A script to populate the database with dummy data."""
+"""A script to populate the database with test data."""
 
 
 def reset_database():
     """Drops all tables in the database and recreates them."""
+
+    def _drop_all():
+        """SQLAlchemy doesn't provide a CASCADE drop_all method, so we have to do it manually."""
+        for table in db.metadata.sorted_tables:
+            db.session.execute(text(f"DROP TABLE IF EXISTS {table.name} CASCADE"))
+            db.session.commit()
+
     with app.app_context():
-        db.drop_all()
+        _drop_all()
         db.create_all()
+    print("Database reset.")
+
+
+def add_test_user():
+    """Adds a test user to the database."""
+    print("Adding test user...")
+    h_password = _hash_password("password")
+    test_user = User(email="example@mail.com", password=h_password)
+    db.session.add(test_user)
+    db.session.commit()
+    print("Test user added.")
+
+
+def get_test_user_id():
+    """Gets the user_id of the test user."""
+    print("Getting test user_id...")
+    return db.session.execute(
+        select(User.id).where(User.email == "example@mail.com")
+    ).first()
+
+
+def add_test_income(test_user_id):
+    print("Adding test income data...")
+    test_income_1 = Income(user_id=test_user_id,
+                           category=IncomeCategory.SALARY,
+                           date="2021-01-01",
+                           frequency=Frequency.MONTHLY,
+                           amount=2000.00,
+                           description="An example of some income")
+    test_income_2 = Income(user_id=test_user_id,
+                           category=IncomeCategory.INTEREST,
+                           date="2021-01-01",
+                           frequency=Frequency.ANNUALLY,
+                           amount=100.00,
+                           description="An example of some interest")
+    db.session.add(test_income_1)
+    db.session.add(test_income_2)
+    db.session.commit()
+    print("Test income data added.")
+
+
+def add_test_expense(test_user_id):
+    print("Adding test expense data...")
+    test_expense_1 = Expense(user_id=test_user_id,
+                             amount=100.00,
+                             description="Test transaction 1",
+                             date="2021-01-01",
+                             category=ExpenseCategory.RENT)
+    test_expense_2 = Expense(user_id=test_user_id,
+                             amount=50.00,
+                             description="Test transaction 2",
+                             date="2021-01-02",
+                             category=ExpenseCategory.MORTGAGE)
+    test_expense_3 = Expense(user_id=test_user_id,
+                             amount=75.00,
+                             description="Test transaction 3",
+                             date="2021-01-03",
+                             category=ExpenseCategory.UTILITIES)
+    test_expense_4 = Expense(user_id=test_user_id,
+                             amount=33.00,
+                             description="Test transaction 4",
+                             date="2021-01-04",
+                             category=ExpenseCategory.UTILITIES)
+    db.session.add(test_expense_1)
+    db.session.add(test_expense_2)
+    db.session.add(test_expense_3)
+    db.session.add(test_expense_4)
+    db.session.commit()
+    print("Test expense data added.")
+
+
+def add_test_budgets(test_user_id):
+    print("Adding test budget data...")
+    test_budget_1 = Budget(user_id=test_user_id,
+                           category=ExpenseCategory.RENT,
+                           frequency=Frequency.MONTHLY,
+                           amount=500.00)
+    test_budget_2 = Budget(user_id=test_user_id,
+                           category=ExpenseCategory.MORTGAGE,
+                           frequency=Frequency.MONTHLY,
+                           amount=300.00)
+    test_budget_3 = Budget(user_id=test_user_id,
+                           category=ExpenseCategory.UTILITIES,
+                           frequency=Frequency.FOUR_WEEKLY,
+                           amount=200.00)
+    db.session.add(test_budget_1)
+    db.session.add(test_budget_2)
+    db.session.add(test_budget_3)
+    db.session.commit()
+    print("Test budget data added.")
 
 
 def main():
     with app.app_context():
+        print("===== Resetting database ====")
         reset_database()
 
-        # Add some dummy data to the database
-        h_password = _hash_password("password")
-        test_user = User(email="example@mail.com", password=h_password)
-        db.session.add(test_user)
-        db.session.commit()
+        print("===== Adding test user(s) to the database =====")
+        add_test_user()
+        test_user_id = get_test_user_id()[0]
+        print(f"Test user_id: {test_user_id}")
 
-        test_user_id = db.session.execute(
-            select(User.id).where(User.email == "example@mail.com")
-        ).first()
-        print(f"TEST_USER_ID_HERE =============== {test_user_id}")
+        print("===== Adding test income to the database =====")
+        add_test_income(test_user_id)
 
-        test_transaction_1 = Transaction(user_id=test_user_id[0], amount=100.00,
-                                         description="Test transaction 1", date="2021-01-01", category=CategoryName.RENT)
-        test_transaction_2 = Transaction(user_id=test_user_id[0], amount=50.00,
-                                         description="Test transaction 2", date="2021-01-02", category=CategoryName.MORTGAGE)
-        test_transaction_3 = Transaction(user_id=test_user_id[0], amount=75.00,
-                                         description="Test transaction 3", date="2021-01-03", category=CategoryName.UTILITIES)
-        db.session.add(test_transaction_1)
-        db.session.add(test_transaction_2)
-        db.session.add(test_transaction_3)
-        db.session.commit()
+        print("===== Adding test expenses to the database =====")
+        add_test_expense(test_user_id)
 
-        test_budget_1 = Budget(user_id=test_user_id[0], category=CategoryName.RENT, amount=500.00)
-        test_budget_2 = Budget(user_id=test_user_id[0], category=CategoryName.MORTGAGE, amount=300.00)
-        test_budget_3 = Budget(user_id=test_user_id[0], category=CategoryName.UTILITIES, amount=200.00)
-        db.session.add(test_budget_1)
-        db.session.add(test_budget_2)
-        db.session.add(test_budget_3)
-        db.session.commit()
+        print("===== Adding test budgets to the database =====")
+        add_test_budgets(test_user_id)
 
+        print("===== Database population complete =====")
+        print("Closing database connection...")
         db.session.close()
+        print("Database connection closed.")
+
+        print("===== Exiting populate.py =====")
 
 
 if __name__ == "__main__":
