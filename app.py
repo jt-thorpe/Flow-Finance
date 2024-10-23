d7d419c8ad60ef14dac4c97796d6bde6049cdbbd
jt-thorpe <jtt-github@proton.me>
2024-10-23 13:12:06 +0100

    feat: initial work on using caching for dashboard
    - when user authenticates, using the User object we retrieve all associated incomes, expenses and budgets which we then put into a redis cache
    - some initial work on then manipulating this cached data for the Dashboard rather then making additional queries to the database
    - added a new service to handle the caching of user data
    - add queries for calculating budget_summaries

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
+import json
 import os
+from datetime import timedelta
 
+import redis
 from flask import (Flask, jsonify, make_response, render_template, request,
-                   url_for)
+                   session, url_for)
 from sqlalchemy.exc import IntegrityError
 
 from extensions import db
 from flow.backend.authentication.auth import (authenticate, generate_token,
                                               login_required,
                                               register_user_account)
-from flow.backend.postgresql.queries import get_budgets_by, get_n_transactions
+from flow.backend.postgresql.queries import create_budget_summary
+from flow.backend.services.user_services import (get_user_with_associations,
+                                                 serialise_user_associations)
 
 app = Flask(__name__,
             template_folder='/app/flow/frontend/templates/',
             static_folder='/app/flow/frontend/static/')  # prepend /app/ for Docker
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['FLOW_DB_URI']
+
+app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']  # for session
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['FLOW_DB_URI']  # for PSQL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+
+
+# Use the Redis service hostname from docker-compose (it’s called 'redis')
+redis_host = os.getenv('REDIS_HOST', 'redis')
+redis_cache = redis.Redis(host=redis_host, port=6379, decode_responses=True)
+CACHE_EXPIRATION = 60 * 30
+
 
 db.init_app(app)
 
@@ -31,7 +46,6 @@ def register_serve() -> str:
 @app.route('/register', methods=['POST'])
 def register_user():
     data = request.json
-
     try:
         register_user_account(data['email'], data['password'])
         return jsonify({'success': True, 'redirect': url_for('login_serve')}), 200
@@ -42,7 +56,6 @@ def register_user():
 @app.route('/login', methods=['GET'])
 def login_serve() -> str:
     """Serve `login.html` page."""
-    # TODO: Implement some kind of check for a token etc so don't always have to login if token valid
     return render_template('login.html')
 
 
@@ -50,14 +63,32 @@ def login_serve() -> str:
 def login_authenticate():
     data = request.json
     user_id = authenticate(data['email'], data['password'])
-    print(f"USER =============== {user_id}")
 
     if user_id:
-        token = generate_token(user_id=user_id)
-        print(f"TOKEN =============== {token}")
+        try:
+            # Load the user object with associated data
+            user = get_user_with_associations(user_id=user_id)
+
+            user_json = json.dumps(
+                serialise_user_associations(user)
+            )
+
+            redis_cache.set(f"user:{session['user_id']}", user_json, ex=CACHE_EXPIRATION)
+        except Exception as e:
+            return jsonify({'success': False, 'message': f'Unable to cache user data: {e}'}), 500
+
+        # Generate the token
+        token = generate_token(user_id=user.id)
+
+        # Cache user data in the session
+        session['user_id'] = str(user.id)
+        session['user_alias'] = user.alias
+
+        # Set the token in a cookie
         response = make_response(jsonify({'success': True, 'redirect': url_for('dashboard_serve')}), 200)
         response.set_cookie('jwt_token', token, httponly=True, secure=True, samesite='Strict')
         return response
+
     return jsonify({'success': False, 'message': 'Invalid credentials'}), 401
 
 
@@ -65,9 +96,14 @@ def login_authenticate():
 @login_required
 def dashboard_serve() -> str:
     """Serve `dashboard.html` page."""
+    cached_user_data = json.loads(redis_cache.get(f"user:{session['user_id']}"))
+    print(f"DEBUG cached_user_data = {cached_user_data}")
+    print(f"DEBUG cached_user_data['expenses'] = {cached_user_data['expenses']}")
+
     return render_template('dashboard.html',
-                           transactions=get_n_transactions(user_id=request.user_id, N=10),
-                           budgets=get_budgets_by(request.user_id))
+                           user_alias=session["user_alias"],
+                           transactions=cached_user_data["user_expenses"],
+                           budget_summary=create_budget_summary(session["user_id"]))
 
 
 if __name__ == '__main__':
